0dc30341147fafdab4ff52ef4c5a515344704a02
sulthanqintara <sulthanqintara@gmail.com>
2021-10-25 01:46:14 +0700

    undo fix

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql");
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
-const db = mysql.createPool({
-  connectionLimit: 10,
+const db = mysql.createConnection({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
